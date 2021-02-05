b9c51239d7ca11b6302341b8653840d7eecadb8a
srr2k21 <78545353+srr2k21@users.noreply.github.com>
2021-02-05 20:03:34 +0530

    Add files via upload
    week 1

---
+++ 1qs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int a ,b ,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
+
+}
