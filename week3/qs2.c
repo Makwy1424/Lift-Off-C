9be5c4a39a52617a204f12923d8406b5d7c1d428
srr2k21 <78545353+srr2k21@users.noreply.github.com>
2021-03-09 15:20:10 +0530

    Add files via upload

---
+++ week3/qs2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int factorial(int n)
+{
+    int temp = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        temp = temp * i;
+    }
+    return temp;
+}
+int main()
+{
+    int a;
+    printf("Enter a number \n");
+    scanf("%d", &a);
+    printf("The Factorial of %d is %d",a, factorial(a));
+    return 0;
+}
